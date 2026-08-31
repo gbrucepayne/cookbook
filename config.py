7ad6da4f03737a3ccf99340199cfbc02edd7d73d
gbrucepayne <gbrucepayne@hotmail.com>
2026-08-30 21:54:13 -0400

    update config/env

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ class Config:
     BASE_DIR = os.path.abspath(os.path.dirname(__file__))
     SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.join(BASE_DIR, 'recipes.db')}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.environ.get('COOKBOOK_KEY', 'devkey')
+    SECRET_KEY = os.getenv('COOKBOOK_KEY', 'devkey')
     IMAGE_FOLDER = os.path.join(BASE_DIR, 'app', 'static', 'images')
     MAX_CONTENT_LENGTH = 16 * 1024 * 1024
     ALLOWED_EXTENSIONS: ClassVar[set[str]] = {'png', 'jpg', 'jpeg', 'webp', 'gif', 'heic', 'heif'}
--- run.py
+++ run.py
@@ -1,7 +1,13 @@
+import os
+
+from dotenv import load_dotenv
+
 from app import create_app
+
+load_dotenv()
 
 app = create_app()
 
 if __name__ == '__main__':
     # Using Port 5001 to guarantee macOS compatibility without AirPlay conflicts
-    app.run(host='0.0.0.0', port=5001, debug=True)
+    app.run(host='0.0.0.0', port=int(os.getenv('PORT', '5001')), debug=True)
