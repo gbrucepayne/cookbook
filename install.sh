902103e8b31f8b65cde781d97cf1b24b264d35c8
gbrucepayne <gbrucepayne@hotmail.com>
2026-09-10 13:44:41 -0400

    attempt to resolve tmp folder size

--- install.sh
+++ install.sh
@@ -37,7 +37,7 @@ source .venv/bin/activate
 echo "$TAG Upgrading pip..."
 pip install --upgrade pip
 echo "$TAG Installing Python dependencies..."
-pip install -r requirements.txt --timeout 120 --no-cache-dir
+TMPDIR=/opt/cookbook/pip_tmp pip install -r requirements.txt --timeout 120 --no-cache-dir
 
 echo "$TAG Initializing database..."
 export FLASK_APP=run.py
